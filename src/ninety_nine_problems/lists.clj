c8ae8e8400030b9185295027ba51ab2e79ab0845
Albert Llousas <albert.llousas@n26.com>
2021-10-22 10:00:01 +0200

    First commit

---
+++ src/ninety_nine_problems/lists.clj
@@ -0,0 +1,80 @@
+(ns ninety-nine-problems.lists)
+
+;; all solutions without the use of clojure built-in fns to operate with lists
+
+;; P01
+(defn find-the-last-element
+  [[first & remaining]]
+  (cond
+    (empty? remaining) first
+    :else (recur remaining)))
+
+;; P02
+(defn find-the-last-but-one-element
+  [[first second & remaining]]
+  (cond
+    (and (nil? second) (empty? remaining)) nil
+    (empty? remaining) first
+    :else (recur (conj remaining second))))
+
+;; P03
+(defn find-the-kth-element
+  [[head & remaining] k]
+  (cond
+    (and (nil? head) (empty? remaining)) nil
+    (= k 1) head
+    :else (recur remaining (dec k))))
+
+;; P04
+(defn count'
+  [coll]
+  (letfn
+    [(inner
+       [[head & tail] counter]
+       (if (nil? head)
+         counter
+         (recur tail (inc counter))))]
+    (inner coll 0)))
+
+;; P05
+(defn reverse'
+  [[head & tail]]
+  (if (nil? head) () (concat (reverse' tail) (list head))))
+
+;; P05 tail-recursive
+(defn reverse''
+  [coll]
+  (letfn
+    [(inner
+       [[head & tail] acc]
+       (if (nil? head)
+         acc
+         (recur tail (concat (list head) acc))))]
+    (inner coll ())))
+
+;; P06
+(defn is-palindrome [coll] (= coll (reverse' coll)))
+
+;; P07
+(defn flatten'
+  [coll]
+  (letfn
+    [(inner
+       [[head & tail] acc]
+       (cond
+         (nil? head) acc
+         (seq? head) (recur tail (flatten' (concat acc head)))
+         :else (recur tail (concat acc (list head)))))]
+    (inner coll ())))
+
+;; P08
+(defn eliminate-consecutive-duplicates
+  [coll]
+  (letfn
+    [(inner
+       [[first second & tail :as all] acc]
+       (cond
+         (empty? all) acc
+         (= first second) (recur tail (concat acc (list first)))
+         :else (recur tail (concat acc (list first second)))))]
+    (inner coll ())))
---
+++ test/ninety_nine_problems/lists_test.clj
@@ -0,0 +1,64 @@
+(ns ninety-nine-problems.lists-test
+  (:require [clojure.test :refer :all])
+  (:require [ninety-nine-problems.lists :refer :all]))
+
+(def empty-list ())
+
+(deftest find-the-last-element-test
+  (testing "should find nothing when the list is empty"
+    (is (= nil (find-the-last-element empty-list))))
+  (testing "should find the only element for a singleton list"
+    (is (= :a (find-the-last-element '(:a)))))
+  (testing "should find the last element for a list of more than one element"
+    (is (= :c (find-the-last-element '(:a :b :c))))))
+
+(deftest find-the-last-but-one-element-test
+  (testing "should find nothing when the list is empty"
+    (is (= nil (find-the-last-but-one-element empty-list))))
+  (testing "should find nothing for a singleton list"
+    (is (= nil (find-the-last-but-one-element '(:a)))))
+  (testing "should find the last element but one for a list of more than one element"
+    (is (= :b (find-the-last-but-one-element '(:a :b :c))))))
+
+(deftest find-the-kth-element-test
+  (testing "should find nothing when the list is empty"
+    (is (= nil (find-the-kth-element empty-list 1))))
+  (testing "should find nothing for the list is shorter than k"
+    (is (= nil (find-the-kth-element '(:a) 10))))
+  (testing "should find the k element for a list"
+    (is (= :b (find-the-kth-element '(:a :b :c) 2)))))
+
+(deftest count-test
+  (testing "should count zero when a list is empty"
+    (is (= 0 (count' empty-list))))
+  (testing "should count the number of elements of a non-empty list"
+    (is (= 3 (count' '(:a :b :c))))))
+
+(deftest reverse-test
+  (testing "should do nothing for an empty list"
+    (is (and
+           (= (reverse' empty-list) empty-list)
+           (= (reverse'' empty-list) empty-list))))
+  (testing "should do nothing for a non-empty list"
+    (is (and
+          (= (reverse' '(:a :b :c)) '(:c :b :a))
+          (= (reverse'' '(:a :b :c)) '(:c :b :a))))))
+
+(deftest is-palindrome-test
+  (are [expected i] (= expected (is-palindrome i))
+                    true (seq "racecar")
+                    true '(:a :b :b :a)
+                    true '()
+                    false (seq "albert")))
+
+(deftest flatten-test
+  (testing "should flatten a inner nested list"
+    (is (= (flatten' (list :a (list :b :c) :d :e)) '(:a :b :c :d :e))))
+  (testing "should flatten a multiple nested lists"
+    (is (= (flatten' (list :a (list :b :c) (list (list :d)  (list :e)))) '(:a :b :c :d :e))))
+  (testing "should do nothing for an already flattened list"
+    (is (= (flatten' '(:a :b :c :d :e)) '(:a :b :c :d :e)))))
+
+(deftest eliminate-consecutive-duplicates-test
+  (testing "should do nothing for an empty list"
+    (is (= (eliminate-consecutive-duplicates '())) '())))
